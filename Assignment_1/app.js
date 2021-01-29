d939c9a33461e2eaff356341bae3a474847477d8
yeeteing <ytlo@mun.ca>
2021-01-28 23:10:05 -0330

    added feature on add, get for books and loans

--- Assignment_1/app.js
+++ Assignment_1/app.js
@@ -6,31 +6,31 @@ const port = "3000"
 app.use(bodyParser.json()); // support json encoded bodies
 
 // Add a new book.
-app.post('/book', library.add);
+app.post('/book', library.addBook);
 
 // Retrieve the information of a book by id
-app.get('/bookInfo/:id', library.get_contact);
+app.get('/bookInfo/:id', library.getBookInfo);
 
 // List all books, providing the book id and its name.
-app.get('/listAllBooks', library.list_all);
+app.get('/listAllBooks', library.listAllBooks);
 
 // List all books by a given year with all its information.
-app.get('/listAllBooks/:year', library.list_all);
+app.get('/listAllBooks/:year', library.listAllBooksByYear);
 
 // Verify if a book is available.
-app.get('/bookAvailibility/:id', library.list_all);
+app.get('/bookAvailibility/:id', library.bookAvailibility);
 
 // Add a loan.
-app.post('/loan', library.add);
+app.post('/loan', library.addLoan);
 
 // List all loans that were finished (the book was returned).
-app.get('/listAllLoans/:returned', library.list_all);
+app.get('/listAllLoans/returned', library.listAllReturnedLoans);
 
 // List all loans that are open (the book was not returned).
-app.get('/listAllLoans/:unreturned', library.list_all);
+app.get('/listAllLoans/notReturned', library.listAllNotReturnedLoans);
 
 // Update a loan. This operation should control if the book was returned or not.
-app.put('/loan/:id', library.update_contact);
+app.put('/loan/:id', library.updateLoan);
 
 app.listen(port, () => {
   console.log('Example app listening at http://localhost:%d', port);
--- Assignment_1/library.js
+++ Assignment_1/library.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const { cpuUsage } = require('process');
 const v = require('./validate-fields');
 
 //path to file/database
-const booksdDb = './database/booksDb.json';
+const booksDb = './database/booksDb.json';
 const loansDb = './database/loansDb.json';
 
 /**
@@ -14,104 +15,201 @@ const loansDb = './database/loansDb.json';
  */
 
 // Add a new book.
-// Retrieve the information of a book by id.
-// List all books, providing the book id and its name.
-// List all books by a given year with all its information.
-// Verify if a book is available.
-// Add a loan.
-// List all loans that were finished (the book was returned).
-// List all loans that are open (the book was not returned).
-// Update a loan. This operation should control if the book was returned or not.
-module.exports.add = async (req, res) => {
+const addBook = async (req, res) => {
     const name = req.body.name;
     const authors = req.body.authors;
     const year = req.body.year;
     const publisher = req.body.publisher;
     const isValid = true /* await v.validate_fields(id, name, authors, year, publisher); */
-    
-    if (isValid){
-        var id;
-        var books;
+
+    if (isValid) {
         //get the last book id
-        fs.readFile(booksdDb, {encoding:'utf8', flag:'r'}, (err, data) => {
+        fs.readFile(booksDb, (err, data) => {
             if (err) throw err;
-            books = JSON.parse(data).books;
-            console.log(`books: ${books}`)
-            id = books.length;
-            console.log(`id: ${id}`)
-            //add to the file with the new id
+
+            var newId;
+            var books;
+            // convert to object
+            const jsonData = JSON.parse(data);
+            books = jsonData.books;
+
+            // create a new id for the new book 
+            newId = books.length + 1;
+
             let newBook = {
-                id: id,
+                id: newId,
                 name: name,
                 authors: authors,
                 year: year,
                 publisher: publisher
             };
-            
+
+            //add the new book to the books array
             books.push(newBook)
-            let allBooks = {
+
+            // convert to object 
+            const allBooks = {
                 books
             }
-            console.log(`books: ${JSON.stringify(allBooks)}`)
-            fs.writeFileSync(booksdDb, JSON.stringify(allBooks));
+
+            //replace the json file with new list of books
+            fs.writeFileSync(booksDb, JSON.stringify(allBooks));
             res.send('Books are correctly inserted in the file.');
         });
-       
     } else {
         res.send('Error. User not inserted in the file.');
     }
 };
 
+// Retrieve the information of a book by id.
+const getBookInfo = async (req, res) => {
+    //get the book id from parameter, and convert it to number
+    const id = parseInt(req.params.id);
 
-/**
- * A function that lists all contacts with all information that is
- * in the file. 
- * @param {*} req 
- * @param {*} res 
- */
-module.exports.list_all = (req, res) => {
-    fs.readFile(file_name, {encoding:'utf8', flag:'r'}, function(err, data){
+    fs.readFile(booksDb, (err, data) => {
         if (err) throw err;
-        res.send(data);
-    });
-};
+        // convert to object
+        const jsonData = JSON.parse(data);
+        var books = jsonData.books;
 
-/**
- * An auxiliary function to locate the line that contains the 
- * requested contact name. 
- * @param {String} contact_name: The contact name to be found in the file
- * @param {Array} lines: The lines from the file
- */
-var _find_contact_index = (contact_name, lines) => {
-    for (let index = 0; index < lines.length; index++){
-        let fields = lines[index].split(',');
-        if (fields[0] == contact_name){
-            return index;
+        let bookInfo = books.filter((book) => parseInt(book.id) === id);
+        res.send(bookInfo);
+    })
+}
+
+// List all books, providing the book id and its name.
+const listAllBooks = async (req, res) => {
+    fs.readFile(booksDb, (err, data) => {
+        if (err) throw err;
+        // convert to object
+        const jsonData = JSON.parse(data);
+        var books = jsonData.books;
+
+        let newListBook = books.map((book) => {
+            return { id: book.id, name: book.name }
+        });
+
+        const allBooks = {
+            books: newListBook
         }
-    }
-    return null;
-};
-
-/**
- * A function that gets a contact by name and returns all
- * data of the requested contact. 
- * @param {*} req 
- * @param {*} res 
- */
-module.exports.get_contact = (req, res) => {
-    fs.readFile(file_name, {encoding:'utf8', flag:'r'}, function(err, data){
-        if (err) throw err;
-        let lines = data.split('\n');
-        let name_to_match = req.params.name;
-        let contact_idx = _find_contact_index(name_to_match, lines);
-        if (contact_idx != null){
-            res.send(lines[contact_idx]);
-        } else{
-            let msg = 'The user '+name_to_match+' was not found.';
-            res.send(msg);
-        }        
+        res.send(allBooks);
     });
-};
+}
+
+// List all books by a given year with all its information.
+const listAllBooksByYear = async (req, res) => {
+    //get the book year from parameter, and convert it to number
+    const year = parseInt(req.params.year);
+
+    fs.readFile(booksDb, (err, data) => {
+        if (err) throw err;
+        // convert to object
+        const jsonData = JSON.parse(data);
+        var books = jsonData.books;
+
+        let newListBook = books.filter((book) => parseInt(book.year) === year);
+
+        const allBooks = {
+            books: newListBook
+        }
+        res.send(allBooks);
+    });
+}
+
+// Verify if a book is available.
+const bookAvailibility = async (req, res) => {
+    //get the book id from parameter, and convert it to number
+    const bookId = parseInt(req.params.id);
+
+    fs.readFile(loansDb, (err, data) => {
+        if (err) throw err;
+        // convert to object
+        const jsonData = JSON.parse(data);
+        var loans = jsonData.loans;
+
+        let availability = loans.filter((loan) => parseInt(loan.bookId) === bookId && loan.was_returned);
+        res.send(bookInfo);
+    })
+}
+
+// Add a loan.
+const addLoan = async (req, res) => {
+    const bookId = req.body.bookId;
+    const date = req.body.date;
+    const client_name = req.body.client_name;
+    const was_returned = req.body.was_returned;
+    const date_of_return = req.body.date_of_return;
+
+    const isValid = true /* await v.validate_fields(id, name, authors, year, publisher); */
+
+    if (isValid) {
+        //get the last loan id
+        fs.readFile(loansDb, (err, data) => {
+            if (err) throw err;
+
+            var newId;
+            var loans;
+            // convert to object
+            const jsonData = JSON.parse(data);
+            loans = jsonData.loans;
+
+            // create a new id for the new book 
+            newId = loans.length + 1;
+
+            let newLoan = {
+                id: newId,
+                bookId: bookId,
+                date: date,
+                client_name: client_name,
+                was_returned: was_returned,
+                date_of_return: date_of_return
+            };
+
+            //add the new book to the books array
+            loans.push(newLoan)
+
+            // convert to object 
+            const allLoans = {
+                loans
+            }
+
+            //replace the json file with new list of books
+            fs.writeFileSync(loansDb, JSON.stringify(allLoans));
+            res.send('Books are correctly inserted in the file.');
+        });
+    } else {
+        res.send('Error. User not inserted in the file.');
+    }
+}
+
+// List all loans that were finished (the book was returned).
+const listAllReturnedLoans = async (req, res) => { 
+    fs.readFile(loansDb, (err, data) => {
+        if (err) throw err;
+        // convert to object
+        const jsonData = JSON.parse(data);
+        var loans = jsonData.loans;
+
+        let returned = loans.filter((loan) => loan.was_returned);
+        res.send(returned);
+    })
+}
+
+// List all loans that are open (the book was not returned).
+const listAllNotReturnedLoans = async (req, res) => {
+    fs.readFile(loansDb, (err, data) => {
+        if (err) throw err;
+        // convert to object
+        const jsonData = JSON.parse(data);
+        var loans = jsonData.loans;
+
+        let notReturned = loans.filter((loan) => !loan.was_returned);
+        res.send(notReturned);
+    })
+ }
+
+// Update a loan. This operation should control if the book was returned or not.
+const updateLoan = async (req, res) => { }
 
 /**
  * A function to update the information about a given contact.
@@ -119,54 +217,30 @@ module.exports.get_contact = (req, res) => {
  * @param {*} res 
  */
 module.exports.update_contact = (req, res) => {
-    let update_contact = req.body.name+','+req.body.email+','+req.body.tel+','+req.body.address+'\n';
-    fs.readFile(file_name, {encoding:'utf8', flag:'r'}, function(err, data){
+    let update_contact = req.body.name + ',' + req.body.email + ',' + req.body.tel + ',' + req.body.address + '\n';
+    fs.readFile(file_name, { encoding: 'utf8', flag: 'r' }, function (err, data) {
         if (err) throw err;
         let lines = data.split('\n');
         file_out = '';
         let name_to_match = req.params.name;
         let contact_idx = _find_contact_index(name_to_match, lines);
-        if (contact_idx != null){
-            for (let index = 0; index < lines.length; index++){
-                if (index != contact_idx[0]){
-                    file_out += lines[index]+'\n';
-                }else {
+        if (contact_idx != null) {
+            for (let index = 0; index < lines.length; index++) {
+                if (index != contact_idx[0]) {
+                    file_out += lines[index] + '\n';
+                } else {
                     file_out += update_contact;
                 }
-            }            
+            }
             fs.writeFileSync(file_name, file_out);
             res.send('Contact correctly updated.');
-        }else {
-            let msg = 'The user '+name_to_match+' was not found.';
+        } else {
+            let msg = 'The user ' + name_to_match + ' was not found.';
             res.send(msg);
         }
-        
+
     });
 };
 
-/**
- * A function that deletes the information about a given contact.
- * @param {*} req 
- * @param {*} res 
- */
-module.exports.delete_contact = (req, res) => {
-    fs.readFile(file_name, {encoding:'utf8', flag:'r'}, function(err, data){
-        if (err) throw err;
-        let lines = data.split('\n');
-        file_out = '';
-        let name_to_match = req.params.name;
-        let contact_idx = _find_contact_index(name_to_match, lines);
-        if (contact_idx != null){
-            for (let index = 0; index < lines.length; index++){
-                if (index != contact_idx){
-                    file_out += lines[index]+'\n';
-                }
-            }            
-            fs.writeFileSync(file_name, file_out);
-            res.send('Contact correctly deleted.');
-        } else{
-            let msg = 'The user '+name_to_match+' was not found.';
-            res.send(msg);
-        }
-    });
-};
+
+module.exports = { addBook, getBookInfo, listAllBooks, listAllBooksByYear, bookAvailibility, addLoan, listAllReturnedLoans, listAllNotReturnedLoans, updateLoan }
